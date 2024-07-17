b8f3a81acff187c1e101184ba88dcdf302d6ff5c
[SW6] 정찬영 <jcy8828@gmail.com>
2024-07-17 19:33:35 +0900

    modify

--- src/Home/Home2.jsx
+++ src/Home/Home2.jsx
@@ -12,6 +12,40 @@ import {
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
 
 export const Home2 = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [isEditing, setIsEditing] = useState(null);
+  const [currentTodo, setCurrentTodo] = useState("");
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, { text: newTodo }]);
+      setNewTodo("");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") handleAddTodo();
+  };
+
+  const handleDeleteClick = (index) => {
+    const updateTodo = todos.filter((_, todoIndex) => todoIndex !== index);
+    setTodos(updateTodo);
+  };
+
+  const handleEditClick = (index) => {
+    setIsEditing(index);
+    setCurrentTodo(todos[index].text);
+  };
+
+  const handleEditChange = (e) => {
+    setCurrentTodo(e.target.value);
+  };
+
   return (
     <div>
       <div className="title-container">
@@ -21,35 +55,58 @@ export const Home2 = () => {
         <div className="title">TodoList</div>
       </div>
       <div className="todolist-content">
-        <div className="todolist-container">
-          <div className="checkbox">
-            <FontAwesomeIcon icon={faSquareCheck} className="icon" />
-            <input
-              type="text"
-              value="호호"
-              spellcheck="false"
-              size="20"
-              maxlength="16"
-            />
-          </div>
-          <div>
-            <FontAwesomeIcon icon={faPencil} className="icon" />
-            <FontAwesomeIcon icon={faTrashCan} className="icon" />
-          </div>
-        </div>
+        {todos.length === 0 ? (
+          <p>할일을 입력해 주세요</p>
+        ) : (
+          todos.map((todo, index) => (
+            <div key={index} className="todolist-container">
+              <div className="checkbox">
+                <FontAwesomeIcon icon={faSquareCheck} className="icon" />
+                {isEditing === index ? (
+                  <input
+                    type="text"
+                    size="20"
+                    maxLength="16"
+                    value={currentTodo}
+                    onChange={handleEditChange}
+                    autoFocus
+                  />
+                ) : (
+                  <div>{todo.text}</div>
+                )}
+              </div>
+              <div>
+                <FontAwesomeIcon
+                  icon={faPencil}
+                  className="icon"
+                  onClick={() => handleEditClick(index)}
+                />
+                <FontAwesomeIcon
+                  icon={faTrashCan}
+                  className="icon"
+                  onClick={() => handleDeleteClick(index)}
+                />
+              </div>
+            </div>
+          ))
+        )}
       </div>
       <div className="createitem-container">
         <div className="createitem-icon">
           <FontAwesomeIcon icon={faStar} />
           <input
             type="text"
-            spellcheck="false"
             size="20"
-            maxlength="16"
+            maxLength="16"
             placeholder="할 일을 입력해주세요"
+            value={newTodo}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
           />
         </div>
-        <button className="save">저장</button>
+        <button className="save" onClick={handleAddTodo}>
+          저장
+        </button>
       </div>
     </div>
   );
